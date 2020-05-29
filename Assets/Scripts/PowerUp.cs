66d24b6574643dc311378f15ae51a662d6c227f6
Omar Pino <matato_357@hotmail.com>
2020-05-28 20:50:20 -0700

    PowerUpAdvance

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,18 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
-public class PowerUp : MonoBehaviour
+public abstract class PowerUp : MonoBehaviour
 {
-    [SerializeField] float duration;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float durationOfPowerUp = 3f;
+    [SerializeField] float lifetimeOfPowerUp = 3f;
+
+    private void Start()
     {
-        
+        StartCoroutine(DestroyAfterLifetime());
+    }
+    // Start is called before the first frame update
+    public virtual void CollisionEffect(GameObject collidedWith)
+    {
+        StartCoroutine(PowerEffect(collidedWith));
     }
 
-    // Update is called once per frame
-    void Update()
+    public abstract void StartEffect(GameObject playerObject);
+
+    public abstract void EndEffect(GameObject playerObject);
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CollisionEffect(collision.gameObject);
+        }
+    }
+
+    IEnumerator PowerEffect(GameObject player)
+    {
+        StopCoroutine(DestroyAfterLifetime());
+        GetComponent<Renderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        StartEffect(player);
+        yield return new WaitForSeconds(durationOfPowerUp);
+        EndEffect(player);
+        Destroy(gameObject);
+    }
+
+    IEnumerator DestroyAfterLifetime()
+    {
+        yield return new WaitForSeconds(lifetimeOfPowerUp);
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/ShieldPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ShieldPowerUp : PowerUp
+{
+    [SerializeField] private GameObject shieldPrefab;
+    private GameObject shieldInstance;
+    public override void EndEffect(GameObject playerObject)
+    {
+        shieldInstance = Instantiate(shieldPrefab, playerObject.transform);
+        shieldInstance.transform.localScale = playerObject.transform.localScale * 1.5f;
+    }
+
+    public override void StartEffect(GameObject playerObject)
+    {
+        Destroy(shieldInstance);
+    }
+
+}
